bf4107f7fafd7b2f0dbf9ddc1bcfb2f2f8fe2cd2
Sohan Nair <sohannair25@gmail.com>
2024-06-03 05:36:14 -0600

    created nav bar and hero

---
+++ _themes_/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens : {
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        transparentIndigoBlue: "#05111c",
+        darkerTransparentIndigoBlue: "#030c13"
+      }
+    },
+  },
+  plugins: [],
+}
+
