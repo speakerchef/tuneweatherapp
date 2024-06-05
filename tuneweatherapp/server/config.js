49b4eb15396dbf49a45203f087e8d7566e9a0c90
Sohan Nair <sohannair25@gmail.com>
2024-06-04 22:34:40 -0600

    trying different approach

--- tuneweatherapp/server/config.js
+++ tuneweatherapp/server/config.js
@@ -1,5 +1,5 @@
 export const OPENAI_API_KEY = "sk-proj-lKIBXlJGJI0lQ7CgzB9gT3BlbkFJ9ptXykabwGdtgHOfJZPH"
-export const SPOTIFY_AUTH_TOKEN = "BQAYwQQTtmWy4DnusAlmobmLwEU4wZOF4mVnZnvSRzVkcsQeZ2d5Q3byMgo8uOHctVdQNhMEGTvqtskg50QRCu6fa55M1-5fEJ57Vg0oACRTAwmGNOGNTzVX6jCDTEsmCiV8nuGAicXslwz4dTgRuyYmhGffpGpWkA2VJescgpwkCSlAx6VlrK2mjnlEQOD-p6w2YbNlnGIhV0Ye9s9sbCowZESSgO1sV-QzdMjYCu2GCIJZAdyntLO2N7r9MQ3MAfWBC2lRwlEOiDHkNoWEsjb2"
+export const SPOTIFY_AUTH_TOKEN = "BQDIHSYNYGhgh8duI-hjmFrOePo7rzM3vDDOGdTkVOlwwEl7IVE-l8-sSAIlFq2d8lIeRr9uNVbrEHPl5NqQm9tryL3q7p2ogjZ5e4sV2L7qPrN6Rj9DILCarqZ33jYz59IBhCQzSL2eWSHmz-Z0xsleNSqaSE1fSA70jFQe4Ge5GyHPB9EuKU71jz4UNNqnnodVzPdgz_iftqYr4e36A4yto5DRMlUvXGxuKkipiidBSBtMuUpf5vAjKRX6cEMs4aQlLGWyezRqcN-J5Gh1d2zj"
 export const WEATHERAPI_TOKEN = "1459eb711f3843df97844058242805"
 export const SPOTIFY_CLIENT_ID = "a4fcad31b33a473990e70cb0594be641"
 export const SPOTIFY_CLIENT_SECRET = "1d9ff66edb394d6982c1ac9bec0339d8"
@@ -7,3 +7,37 @@ export const SPOTIFY_CLIENT_SECRET = "1d9ff66edb394d6982c1ac9bec0339d8"
 /*
 mock audio features url: https://71242a6c-2412-43c5-ae45-0cc2b404d9ab.mock.pstmn.io
 */
+
+
+
+// res.redirect(`?${querystring.stringify({
+//     response_type: "code",
+//     client_id: client_id,
+//     scope: scope,
+//     redirect_uri: redirect_uri,
+// })}`)
+// console.log(req.body)
+//
+// try{
+//     app.get('/', (req, res) => {
+//         const authCode = req.query.code
+//
+//         const authOption = {
+//             url: 'https://accounts.spotify.com/api/token',
+//             form: {
+//                 code: authCode,
+//                 redirect_uri: redirect_uri,
+//                 grant_type: 'authorization_code',
+//             },
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded',
+//                 Authorization: `Basic ${new Buffer.from(client_id + ':' + client_secret).toString('base64')}`,
+//             },
+//             json: true
+//         }
+//     })
+// }catch(err){
+//     console.log(err.response)
+// }
+//
+// console.log(res.body)
--- tuneweatherapp/server/server.js
+++ tuneweatherapp/server/server.js
@@ -5,13 +5,64 @@ import {SPOTIFY_CLIENT_SECRET} from "./config.js";
 import express from "express";
 import cors from "cors";
 import {getWeatherConditions} from "./Components/weather-mood-info.js";
+import * as querystring from "node:querystring";
+import session from 'express-session'
 
 const PORT = process.env.PORT || 5001;
 const app = express();
 const currentDate = new Date()
 
+app.use(express.static('public'));
+
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
+
+
+// GET User auth token
+const client_id = "a4fcad31b33a473990e70cb0594be641"
+const client_secret = "1d9ff66edb394d6982c1ac9bec0339d8"
+const redirect_uri = 'http://localhost:5001/callback'
+
+
+
+
+app.get('/login', (req, res)=>{
+    const authUrl = 'https://accounts.spotify.com/authorize'
+    const scope = 'user-read-email playlist-modify-private'
+
+
+
+    res.redirect(`${authUrl}?${querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+    })}`)
+    console.log(req.body)
+
+
+})
+
+// app.get('/callback', (req, res) => {
+//     const authCode = req.query.code
+//
+//     const authOptions = {
+//         url: 'https://accounts.spotify.com/api/token',
+//         method: 'POST',
+//         form: {
+//             code: authCode,
+//             redirect_uri: redirect_uri,
+//             grant_type: 'authorization_code',
+//         },
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded',
+//             Authorization: `Basic ${new Buffer.from(client_id + ':' + client_secret).toString('base64')}`,
+//         },
+//         json: true
+//     }
+// })
+
+
 
 // Get from spotify API
 const fetchSpotifyApi = async (endpoint) => {
@@ -69,14 +120,18 @@ const getRecommendedTracks = async (seedTracks, danceability, energy, valence, l
 
         const res = fetchSpotifyApi(`v1/recommendations?seed_tracks=${seedTracks.map((trackId, index) => {
             return (index !== seedTracks.length - 1 ? `${trackId}%2C` : trackId)
-        })}&target_danceability=${danceability}&target_energy=${energy}&target_valence=${valence}\`.replaceAll(',','')`, {
+        })}&target_danceability=${danceability}&target_energy=${energy}&target_valence=${valence}`.replaceAll(',',''), {
             header: {
                 Authorization: `Bearer ${SPOTIFY_AUTH_TOKEN}`
             }
         })
         let recommendedTracks = []
         for (let i = 0; i < limit; i++) {
-            recommendedTracks.push({name: (await res).tracks[i].name}, {artist: (await res).tracks[i].album.artists[0].name}, {image: (await res).tracks[i].album.images[1].url}, {link: (await res).tracks[i].external_urls},)
+            recommendedTracks.push({name: (await res).tracks[i].name},
+                {artist: (await res).tracks[i].album.artists[0].name},
+                {image: (await res).tracks[i].album.images[1].url},
+                {link: (await res).tracks[i].external_urls},
+                )
         }
 
         return recommendedTracks
@@ -86,21 +141,21 @@ const getRecommendedTracks = async (seedTracks, danceability, energy, valence, l
     }
 }
 
-const createPlaylist = async(userId) => {
-    try{
-        await axios.post(`https://api.spotify.com/users/${userId}/playlists`, {
-            header:{
-                Authorization: `Bearer ${SPOTIFY_AUTH_TOKEN}`,
+const createPlaylist = async (userId) => {
+    try {
+        const request = await axios.post(`https://api.spotify.com/users/${userId}/playlists`, {
+            header: {
+                Authorization: `Bearer AQBT17rYBwkauIXSA1xnGj0sgGYsYiXZIp4AxoESPiYQIYZEn-tgKHjQvzYknKLOcybWP4w0GGTNymuueo2Koh-caabQBR1bqW9TW-SW-uPCK1mnQhsUoM1f-95v4O-fFh3l6MjGKmXi2fLB2XqLhOJuRsr29bw0f_lNE_cPTItrncOeG6c9DwUYrRowozmEmE0l8F2fOTPyiPucOC2XkFd88g`,
                 "Content-Type": "application/json",
-            },
-            body: {
+            }, data: {
                 "name": `TuneWeatherApp Playlist ${currentDate.toDateString()}`,
                 "description": "A Playlist by the TuneWeather App",
                 "public": false
             }
         })
-    }catch(err){
-        console.log(err.status)
+        console.log(JSON.stringify(await request))
+    } catch (err) {
+        console.log(err.response)
     }
 }
 
@@ -113,6 +168,8 @@ const eg = trackFeatures['audio-features'].energy
 const vl = trackFeatures['audio-features'].valence
 let recommendedTracks = await getRecommendedTracks(arrOfTrackIds, db, eg, vl)
 console.log(recommendedTracks)
+
+await createPlaylist('wokeboydidit')
 
 
 app.listen(PORT, () => {
