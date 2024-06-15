ea4263a6684a9a57538edf3bfb6aba869a401a75
Sohan Nair <sohannair25@gmail.com>
2024-06-15 02:57:48 -0600

    Modified error modal colors

--- tuneweatherapp/client/src/Components/ErrorModal.jsx
+++ tuneweatherapp/client/src/Components/ErrorModal.jsx
@@ -9,10 +9,10 @@ const ErrorModal = ({errorText}) => {
                 </div>
                 <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
                 <div
-                    className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+                    className="inline-block align-bottom bg-[#222] rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
                     role="dialog" aria-modal="true" aria-labelledby="modal-headline">
                     <div>
-                        <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
+                        <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-950 opacity-90">
                             <svg className="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none"
                                  viewBox="0 0 24 24"
                                  stroke="currentColor" aria-hidden="true">
@@ -21,11 +21,11 @@ const ErrorModal = ({errorText}) => {
                             </svg>
                         </div>
                         <div className="mt-3 text-center sm:mt-5">
-                            <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                            <h3 className="text-lg leading-6 font-medium text-red-600" id="modal-headline">
                                 Error
                             </h3>
                             <div className="mt-2">
-                                <p className="text-sm text-gray-500">
+                                <p className="text-sm text-gray-200">
                                     {errorText ? errorText : 'There was an error processing your request. Click \'OK\' to return to the home page.'}
                                 </p>
                             </div>
