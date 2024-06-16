c59fc63101e263f1ab3b4d157cc3e8c710043df0
Sohan Nair <sohannair25@gmail.com>
2024-06-16 06:25:51 -0600

    minor update

--- tuneweatherapp/client/src/Components/ErrorModal.jsx
+++ tuneweatherapp/client/src/Components/ErrorModal.jsx
@@ -34,7 +34,7 @@ const ErrorModal = ({errorText}) => {
                     <div className="mt-5 sm:mt-6">
                         <button
                             className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm"
-                            onClick={() => window.location.replace('/')}>
+                            onClick={() => window.location.replace('https://tuneweather.com')}>
                             OK
                         </button>
                     </div>
