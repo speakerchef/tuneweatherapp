c313bd8a42afd28834716d4a52c66ca6344f73b5
Sohan Nair <sohannair25@gmail.com>
2024-06-16 03:18:25 -0600

    change

--- tuneweatherapp/client/src/pages/Playlist.jsx
+++ tuneweatherapp/client/src/pages/Playlist.jsx
@@ -65,7 +65,6 @@ const Playlist = () => {
           );
           const data = await res.json();
           setApiData(await data.data.city)
-          setLocationLoaded(true);
         } catch (e) {
           console.log("Error sending location", e);
         } finally {
@@ -74,7 +73,10 @@ const Playlist = () => {
       }
     };
 
-    sendUserLocation().then(() => console.log("API DATA", apiData));
+    sendUserLocation().then(() => {
+      console.log("API DATA", apiData)
+      setLocationLoaded(true)
+    });
   }, [latitude, longitude]);
 
   useEffect(() => {
