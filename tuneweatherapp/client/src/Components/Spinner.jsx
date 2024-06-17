1752510ed9dbe5852da551b7a20f7fd79f2d88e0
Sohan Nair <sohannair25@gmail.com>
2024-06-17 11:45:37 -0600

    trying to add gradient to spinner

--- tuneweatherapp/client/src/Components/Spinner.jsx
+++ tuneweatherapp/client/src/Components/Spinner.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { ScaleLoader} from "react-spinners";
+import styled from '@emotion/styled';
 
-const cssOverride = {
-    display: 'block',
-    margin: '0 auto',
-    marginTop: '4rem',
-    marginBottom: '-3rem',
-}
+const GradientScaleLoader = styled(ScaleLoader)`
+  display: block;
+  margin: 0 auto;
+  margin-top: 4rem;
+  margin-bottom: -3rem;
+
+  span {
+    background: linear-gradient(to right, rgb(67 56 202), #e600ff);
+    -webkit-background-clip: text;
+    background-clip: text;
+    color: transparent;
+  }
+`;
 
 const Spinner = ({loading}) => {
     return (
         <div>
             <ScaleLoader
-            color={'#553bff'}
             loading={loading}
             cssOverride={cssOverride}
             size={150}
