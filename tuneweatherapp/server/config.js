5b03ee901f283fe814758bd3c90cd314eba68409
Sohan Nair <sohannair25@gmail.com>
2024-06-17 16:47:53 -0600

    del

--- tuneweatherapp/server/config.js
+++
@@ -1,6 +0,0 @@
-// OPENAI_API_KEY = "sk-proj-Pjd9xZ2m0TPsJ8W9nNy1T3BlbkFJwVwd0lh5CWrGkUaW2tI2"
-// WEATHERAPI_TOKEN = "1459eb711f3843df97844058242805"
-// client_id = "a4fcad31b33a473990e70cb0594be641"
-// client_secret = "c9a3d910e5fb41d7bdbf29243dfb74e3"
-// location_api_key = 'bdc_cf83a333f423403198b4e97735b7becc'
-// session_secret = '8056'
