396f4715bc4ecdce8cdca593220cb2ceec549e4e
Sohan Nair <sohannair25@gmail.com>
2024-06-17 11:34:19 -0600

    testing loader

--- tuneweatherapp/client/src/Components/Spinner.jsx
+++ tuneweatherapp/client/src/Components/Spinner.jsx
@@ -12,7 +12,7 @@ const Spinner = ({loading}) => {
     return (
         <div>
             <ScaleLoader
-            className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-700 to-vibrantMagenta"
+            color={'#553bff'}
             loading={loading}
             cssOverride={cssOverride}
             size={150}
