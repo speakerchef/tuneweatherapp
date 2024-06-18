9115a561490e87941fa16c3e685ee66cfb6add82
Sohan Nair <sohannair25@gmail.com>
2024-06-18 07:51:01 -0600

    added logout button in hamburger menu

--- tuneweatherapp/client/src/components/HamburgerMenu.jsx
+++ tuneweatherapp/client/src/components/HamburgerMenu.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import {FaBook, FaEnvelope, FaGithubSquare} from "react-icons/fa";
+import {FaBook, FaEnvelope, FaGithubSquare, FaSignOutAlt} from "react-icons/fa";
 import {FaHouse} from "react-icons/fa6";
+import LogoutIcon from "./LogoutIcon.jsx";
 
 const HamburgerMenu = ({menuHidden, contactHandler, privacyHandler}) => {
+    const handleLogout = () => {
+        fetch("https://api.tuneweather.com/logout", {
+            method: "DELETE",
+            credentials: "include",
+        }).then(() => {
+            window.location.replace("https://tuneweather.com");
+        });
+    };
+
     return (
         <>
             <div className={`md:hidden flex ${menuHidden && 'hidden'} flex-col top-0 justify-self-center absolute -auto z-20 shadow-2xl py-12 shadow-black mt-12 w-full h- bg-darkerTransparentIndigoBlue overflow-y-auto`}>
@@ -39,6 +49,13 @@ const HamburgerMenu = ({menuHidden, contactHandler, privacyHandler}) => {
                             GitHub
                         </h3>
                     </a>
+                    <button
+                        onClick={handleLogout}
+                        className="p-0 transition-all py-8 duration-100 hover:cursor-pointer px-4 focus:outline-none focus:border-b-4 border-darkMagenta">
+                        <h3>
+                            <FaSignOutAlt className="text-xl mr-1 text-vibrantMagenta inline"/> Logout
+                        </h3>
+                    </button>
                 </div>
             </div>
         </>
