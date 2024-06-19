0a66ba2b74e1b710be31c4675118499656961d43
Sohan Nair <sohannair25@gmail.com>
2024-06-19 08:05:58 -0600

    update

--- tuneweatherapp/client/src/components/Hero.jsx
+++ tuneweatherapp/client/src/components/Hero.jsx
@@ -3,11 +3,11 @@ import Buttons from "./Buttons.jsx";
 
 const Hero = ({mainHeaderHidden, iframeHidden, dashboardHeaderHidden, playlistSectionHeadersHidden, headerText, subHeaderHidden, pid}) => {
   return (
-      <div className="flex flex-col mt-12 bg-transparent mx-auto w-full px-4 overflow-x-hidden">
+      <div className="flex  flex-col mt-12 bg-transparent mx-auto w-full px-4 overflow-x-hidden">
         <section id="hero" className="flex flex-col-reverse my-16 mx-auto">
           <div id="headText" className="flex flex-col items-center justify-between rounded-2xl -my-8 md:-my-4">
             <div className={`${playlistSectionHeadersHidden ? "hidden" : ""}  sm:mx-12 md:mx-8 lg:mx-12 mt-8 mb-16 xl:mx-242`}>
-              <h2 className="text-5xl   font-extrabold bg-gradient-to-br from-lightMagenta to-indigo-700 bg-clip-text text-transparent opacity-85 -my-14 sm:-mx-4 sm:text-5xl slg:text-7xl lg:text-7xl lg:mx-7 md:text-center md:text-6xl lg:text-center xl:text-center xl:text-7xl xxl:text-[5rem] mx-4">
+              <h2 className="text-5xl   font-extrabold bg-gradient-to-br from-lightMagenta to-indigo-700 bg-clip-text text-transparent opacity-85 -my-14 pb-1 sm:-mx-4 sm:text-5xl slg:text-7xl lg:text-7xl lg:mx-7 md:text-center md:text-6xl lg:text-center xl:text-center xl:text-7xl xxl:text-[5rem] mx-4">
                 {headerText ? "Here's your new playlist!" : "Welcome, click the button below to make your first playlist!"}
               </h2>
               <p className={`${!subHeaderHidden && "hidden"} text-center mb-5 mt-4 text-md text-gray-800 mx-4`}>
