c8e8fe2b6d99ee1a0188b20a3845422970c4f278
Sohan Nair <sohannair25@gmail.com>
2024-06-24 10:15:35 +0800

    Settling with semi-fix

--- tuneweatherapp/client/src/pages/Playlist.jsx
+++ tuneweatherapp/client/src/pages/Playlist.jsx
@@ -118,13 +118,15 @@ const Playlist = () => {
   }, [errorCount]);
 
   function iframeReload() {
-    playlistIFrame.current.src = `https://open.spotify.com/embed/playlist/3cEYpjA9oz9GiPac4AsH4n`;
+    playlistIFrame.current.src = `https://open.spotify.com/embed/playlist/${iFrame}`;
     // document.getElementById("playlist-iframe").src = `https://open.spotify.com/embed/playlist/${iFrame}`
     console.log(playlistIFrame.current)
     console.log(document.getElementById("playlist-iframe"))
 
         //= `https://open.spotify.com/embed/playlist/3cEYpjA9oz9GiPac4AsH4n`;
   }
+
+
 
 
 
